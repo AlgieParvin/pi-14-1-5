e2544efe69944f6f4729b6938bc6fda4666d5899
Ramal Yusifov <ramalyusifov@ukr.net>
2016-05-25 19:41:17 +0300

    Work with changes

--- great_guitarists.rb
+++ great_guitarists.rb
@@ -1,4 +1,4 @@
-great_guitarists = ["David Gilmor", "Jimi Hendrix", "Chuck Berry", "Jimmy Page"]
+great_guitarists = ["David Gilmor", "Jimi Hendrix", "Chuck Berry", "Mark Knopfler"]
 great_guitarists.each do |guitarist|
   puts(guitarist + " is a genuis guitarist!")
 end
