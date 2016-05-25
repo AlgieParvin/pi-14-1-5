3994dd0417a1a93ae5297c974711d05e175c4050
Ramal Yusifov <ramalyusifov@ukr.net>
2016-05-25 19:15:51 +0300

    first commit Yusifov

---
+++ great_guitarists.rb
@@ -0,0 +1,4 @@
+great_guitarists = ["David Gilmor", "Jimi Hendrix", "Chuck Berry", "Jimmy Page"]
+great_guitarists.each do |guitarist|
+  puts(guitarist + " is a great guitarist!")
+end
---
+++ us.py
@@ -0,0 +1,12 @@
+from random import randint
+
+WE = ["Ramal Yusifov", "Danya Baranenko", "Nikita Nemtsov", "Slavik Palamar"]
+
+def get_comic_belt():
+   champion = WE[randint(0, 3)]
+   while champion != WE[1] || champion != WE[3]:
+       champion = WE[randint(0, 3)]
+   return "And the champion is ... " + champion.upper()
+
+if __name__ == "__main__":
+    print(get_comic_belt())
