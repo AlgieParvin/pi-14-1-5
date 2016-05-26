f066d441485a2625d8e92ed63b8c7a4c25436ee1
Algie Parvin <pauchok97@gmail.com>
2016-05-26 11:16:38 +0300

    Added Moore to grear guitarists.

--- great_guitarists.rb
+++ great_guitarists.rb
@@ -1,4 +1,4 @@
-great_guitarists = ["David Gilmor", "Jimi Hendrix", "Chuck Berry", "Mark Knopfler"]
+great_guitarists = ["Gary Moore", "David Gilmor", "Jimi Hendrix", "Chuck Berry", "Mark Knopfler"]
 great_guitarists.each do |guitarist|
   puts(guitarist + " is a genuis guitarist!")
 end
